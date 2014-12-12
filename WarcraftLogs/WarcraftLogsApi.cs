0b36f2c52fece9a65af9484d8ee91008dcb6dbf6
Zarania <scott.a.reinhardt@gmail.com>
2014-12-12 11:04:39 -0500

    Added Warcraft Logs project

---
+++ WarcraftLogs/WarcraftLogsApi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WoW.Core.Interfaces;
+
+namespace WarcraftLogs
+{
+    public class WarcraftLogsApi : ILogApi
+    {
+        public const string _baseApiLink = "https://www.warcraftlogs.com/rankings/character_name/{0}/{1}/{2}";
+
+        public WarcraftLogsApi()
+        {
+            
+        }
+
+        public string GetCharacterProfile(string characterName, string server, string region)
+        {
+            return string.Format(_baseApiLink, characterName, server, region);
+        }
+    }
+}
