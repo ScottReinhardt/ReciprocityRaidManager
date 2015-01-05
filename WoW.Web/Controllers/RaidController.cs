20b5c891d4764749597ced6f8ee3fc29cc551afa
Zarania <scott.a.reinhardt@gmail.com>
2015-01-05 16:36:52 -0500

    Added missing controller

---
+++ WoW.Web/Controllers/RaidController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WoW.Core.Interfaces;
+using WoW.Core.Models;
+using WoW.Core.Objects;
+using WoW.Models;
+
+namespace WoW.Controllers
+{
+    public class RaidController : Controller
+    {
+        private readonly ICharacterImporter _importer;
+        private readonly IWoWPersistanceProvider _dataProvider;
+
+        private IEnumerable<Enchant> _enchants;
+
+        public RaidController(ICharacterImporter importer, IWoWPersistanceProvider dataProvider)
+        {
+            _importer = importer;
+            _enchants = _importer.GetEnchants();
+            _dataProvider = dataProvider;
+        }
+
+        public ActionResult RaiderDetails(int raidId)
+        {
+            var raid = _dataProvider.GetRaiderDetails(raidId);
+            
+            return View(raid);
+        }
+
+        public ActionResult Index(int raidId)
+        {
+            var raid = _dataProvider.GetRaiderDetails(raidId);
+            var model = new AddRaiderModel()
+            {
+                Raiders = raid.Raiders,
+            };
+            return View(model);
+        }
+
+        public ActionResult NewRaider(PlayerModel raider)
+        {
+            
+            return new EmptyResult();
+        }
+    }
+}
